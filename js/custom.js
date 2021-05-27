088c7d8b6582b2878fdee1c2b952f70260cb534b
Sergey <40899197+sergeyorloff@users.noreply.github.com>
2021-05-27 20:37:44 +0500

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+/* ПЛАВНОЕ ПОЯВЛЕНИЕ СТРАНИЦЫ ----------------------------------------*/
+/*
+$document.ready(function(){
+$('body').hide().fadeIn(3000);
+});
+$(function(){
+$("#upload_link").on('click', function(e){
+    e.preventDefault();
+    $("#upload:hidden").trigger('click');
+});
+});
+*/
+/*
+$(window).load(function() {
+$('body').fadeIn(3000);
+});
+*/
